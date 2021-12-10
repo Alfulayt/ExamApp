3b6da7965fcbc485ee183ffddb7ccc5cb30a4af3
Alfulayt <admin@des-stu.com>
2021-12-10 18:44:06 +0300

    add devise + setub the template + some basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.find_or_initialize_by(email: 'teacher@example.com')
+user.name = 'teacher of the year'
+user.password = '12345678'
+user.role = 'teacher'
+user.save!
